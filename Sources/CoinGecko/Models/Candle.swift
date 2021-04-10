82945a8c67e69c9582e9188df962403442a2b366
michael <michael.inger@breadwallet.com>
2021-04-10 13:41:21 +0100

    adding public initializers

--- Sources/CoinGecko/Models/Candle.swift
+++ Sources/CoinGecko/Models/Candle.swift
@@ -16,11 +16,19 @@ public struct Candle: Codable {
     public let low: Double
     public let close: Double
 
-    init(arrayData: [Double]) {
+    public init(arrayData: [Double]) {
         date = Date(timeIntervalSince1970: arrayData[0])
         open = arrayData[1]
         high = arrayData[2]
         low = arrayData[3]
         close = arrayData[4]
+    }
+    
+    public init(date: Date, open: Double, high: Double, low: Double, close: Double) {
+        self.date = date
+        self.open = open
+        self.high = high
+        self.low = low
+        self.close = close
     }
 }
